323de90b5b62d22afebd5e0f955b87368fb2d401
sogangCSmaster <cs20131575@u.sogang.ac.kr>
2019-09-17 00:35:21 +0900

    아.. 내일해야지 ㅠㅠ

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const pug = require('pug');
+const methodOverride = require('method-override');
