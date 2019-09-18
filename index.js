ecb085b570e0b77a2cca928d91d7b22204c37bbe
redbird <cs20131575@u.sogang.ac.kr>
2019-09-18 11:24:23 +0900

    main index

--- index.js
+++ index.js
@@ -4,3 +4,56 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const pug = require('pug');
 const methodOverride = require('method-override');
+const session = require('express-session');
+const MYSQLStore = require('express-mysql-session')(session);
+const config = require('./config.json');
+const port = 80;
+
+
+console.log(config);
+
+app.locals.pretty = true;
+app.set('view engine', 'pug');
+app.set('views', `${__dirname}/views`);
+
+app.use(cors());
+app.use(session({
+    secret: "askdfhwehifubsd23fsdkj23",
+    resave: false,
+    saveUninitialized: false,
+    store: new MYSQLStore({
+        host: config.database,
+        port: 3306,
+        user: config.user,
+        password: config.password,
+        database: config.database
+    })
+}));
+
+app.use(express.static(`${__dirname}/public`));
+app.use(bodyParser.urlencoded({ extended: true, limit: '30mb' }));
+app.use(bodyParser.json({ extended: true, limit: '30mb' }));
+
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        var method = req.body._method
+        delete req.body._method
+        return method
+    }
+}))
+
+app.use((req,res,next) => {
+    res.locals.session = req.session;
+    next();
+});
+
+app.get('*', (req, res, next) => {
+    res.status(404).send('404 Error');
+});
+app.get('*', (err, req, res, next) => {
+    res.status(500).send('500 Error');
+});
+
+app.listen(port, () => {
+    console.log("Node server start!");
+})
