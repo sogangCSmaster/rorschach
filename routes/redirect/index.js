df9e1372a5fc7761091b1f9f7bc7d9ac86504c71
sogangCSmaster <cs20131575@u.sogang.ac.kr>
2019-11-18 13:38:19 +0900

    redirect fixed

--- routes/redirect/index.js
+++ routes/redirect/index.js
@@ -6,47 +6,51 @@ const axios = require('axios');
 router.route("/redirect")
     .get(async(req, res, next)=> {
         try{
-            var { m, p, test } = req.query;
-            var api = config.api;
-            api = api + `/memberList.php?m=${m}&p=${p}`;
-            var result = await axios.get(api);
-            result = result.data;
-            if(result.memberID==""){
-                return res.status(400).send("아이디와 비밀번호가 맞지 않습니다. 관리자에게 문의 주세요.")
-            } else {
-                if(test=="test1"){
-                    req.session.user = result;
-                    var user = req.session.user;
-                    var coin1 = user.coin1;
-                    var useCount1 = user.useCount1;
-                    coin1 = Number(coin1);
-                    useCount1 = Number(useCount1);
-                    if(useCount1>=coin1){
-                        return res.send("코인이 부족합니다.");
-                    } else {
-                        req.session.m = m;
-                        req.session.p = p;
-                        return res.redirect("/test1");
-                    }
-                } else if(test=="test2") {
-                    req.session.user = result;
-                    var coin1 = user.coin1;
-                    var useCount1 = user.useCount1;
-                    coin1 = Number(coin1);
-                    useCount1 = Number(useCount1);
-                    if(useCount1>=coin1){
-                        return res.send("코인이 부족합니다.");
-                    } else {
-                        req.session.m = m;
-                        req.session.p = p;
-                        return res.redirect("/test2");
-                    }
-                }
-            }
+            var { m, test } = req.query;
+            res.render('redirect/index', {m, test});
         } catch(err){
             next(err);
         }
+    })
+    .post(async(req, res, next) => {
+        var { user, password, test } = req.body;
+        console.warn(req.body);
+        var api = config.api;
+        api = api + `/memberList.php?m=${user}&p=${password}`;
         
-    });
+        var result = await axios.get(api);
+        result = result.data;
+        console.warn(result);
+        if(result.memberID==""){
+            return res.redirect("/login");
+        } else {
+            req.session.user = result;
+            var user = result;
+            
+            if(test=="test1"){
+                var coin1 = user.coin1;
+                var useCount1 = user.useCount1;
+
+                coin1 = Number(coin1);
+                useCount1 = Number(useCount1);
+                if(useCount1>=coin1){
+                    return res.send("코인이 부족합니다. 충전 후 다시 실행해 주세요.");
+                } else {
+                    return res.redirect('/test1');
+                }
+            } else if (test=="test2"){
+                var coin1 = user.coin2;
+                var useCount1 = user.useCount2;
+
+                coin1 = Number(coin1);
+                useCount1 = Number(useCount1);
+                if(useCount1>=coin1){
+                    return res.send("코인이 부족합니다. 충전 후 다시 실행해 주세요.");
+                } else {
+                    return res.redirect('/test2');
+                }
+            }
+        }
+    })
 
 module.exports = router;
