e92d5021739d62f23f683664280a19e310207231
lll4592 <ljk7776@gmail.com>
2019-11-20 01:57:35 +0900

    fix determinants

--- routes/finishtest/upper_section/blends.js
+++ routes/finishtest/upper_section/blends.js
@@ -2,7 +2,7 @@ function getBlendsArray(scores) {
   // [['F','Ma'], ['FC']] 처럼 표시
   arr = []
   scores.forEach((score) => {
-    if (score.det && Object.keys(score.det) >= 2) {
+    if (score.det && Object.keys(score.det).length >= 2) {
       row = []
       Object.keys(score.det).forEach((key) => {
         row.push(score.det[key].value);
--- routes/finishtest/upper_section/single.js
+++ routes/finishtest/upper_section/single.js
@@ -4,7 +4,7 @@ function countSingleDetWithKey(scores, key, value) {
   // value가 없으면 key만 센다
   let count = 0;
   scores.forEach((score) => {
-    if (score.det && Object.keys(score.det).length === 1 && score.det[key]) {
+    if (score.det && Object.keys(score.det).length >= 1 && score.det[key]) {
       if (value) {
         if (score.det[key].value === value) {
           count += 1;
@@ -19,97 +19,97 @@ function countSingleDetWithKey(scores, key, value) {
 
 function getM(scores) {
   //det의 key size가 1일때
-  return countSingleDetWithKey(scores, 'active1');
+  return countSingleDetWithKey(scores, 'active1') + countSingleDetWithKey(scores, 'passive1') + countSingleDetWithKey(scores, 'a-p1');
 }
 exports.getM = getM;
 
 function getFM(scores) {
-  return countSingleDetWithKey(scores, 'active2');
+  return countSingleDetWithKey(scores, 'active2') + countSingleDetWithKey(scores, 'passive2') + countSingleDetWithKey(scores, 'a-p2');
 }
 exports.getFM = getFM;
 
 function getm(scores) {
-  return countSingleDetWithKey(scores, 'active3');
+  return countSingleDetWithKey(scores, 'active3') + countSingleDetWithKey(scores, 'passive3') + countSingleDetWithKey(scores, 'a-p3');
 }
 exports.getm = getm;
 
 function getFC(scores) {
-  return countSingleDetWithKey(scores, 'chromatic1', 'FC');
+  return countSingleDetWithKey(scores, 'chromatic1');
 }
 exports.getFC = getFC;
 
 function getCF(scores) {
-  return countSingleDetWithKey(scores, 'chromatic1', 'CF');
+  return countSingleDetWithKey(scores, 'chromatic2');
 }
 exports.getCF = getCF;
 
 function getC(scores) {
-  return countSingleDetWithKey(scores, 'chromatic1', 'C');
+  return countSingleDetWithKey(scores, 'chromatic3');
 }
 exports.getC = getC;
 
 function getCn(scores) {
-  return countSingleDetWithKey(scores, 'chromatic1', 'Cn');
+  return countSingleDetWithKey(scores, 'chromatic4');
 }
 exports.getCn = getCn;
 
 function getFCprime(scores) {
-  return countSingleDetWithKey(scores, 'achromatic1', "FC'");
+  return countSingleDetWithKey(scores, 'achromatic3');
 }
 exports.getFCprime = getFCprime;
 
 function getCprimeF(scores) {
-  return countSingleDetWithKey(scores, 'achromatic1', "C'F");
+  return countSingleDetWithKey(scores, 'achromatic2');
 }
 exports.getCprimeF = getCprimeF;
 
 function getCprime(scores) {
-  return countSingleDetWithKey(scores, 'achromatic1', "C'");
+  return countSingleDetWithKey(scores, 'achromatic1');
 }
 exports.getCprime = getCprime;
 
 function getFT(scores) {
-  return countSingleDetWithKey(scores, 'texture1', 'FT');
+  return countSingleDetWithKey(scores, 'texture3');
 }
 exports.getFT = getFT;
 
 function getTF(scores) {
-  return countSingleDetWithKey(scores, 'texture1', 'TF');
+  return countSingleDetWithKey(scores, 'texture2');
 }
 exports.getTF = getTF;
 
 function getT(scores) {
-  return countSingleDetWithKey(scores, 'texture1', 'T');
+  return countSingleDetWithKey(scores, 'texture1');
 }
 exports.getT = getT;
 
 function getFV(scores) {
-  return countSingleDetWithKey(scores, 'vista1', 'FV');
+  return countSingleDetWithKey(scores, 'vista3');
 }
 exports.getFV = getFV;
 
 function getVF(scores) {
-  return countSingleDetWithKey(scores, 'vista1', 'VF');
+  return countSingleDetWithKey(scores, 'vista2');
 }
 exports.getVF = getVF;
 
 function getV(scores) {
-  return countSingleDetWithKey(scores, 'vista1', 'V');
+  return countSingleDetWithKey(scores, 'vista1');
 }
 exports.getV = getV;
 
 function getFY(scores) {
-  return countSingleDetWithKey(scores, 'diffuse1', 'FY');
+  return countSingleDetWithKey(scores, 'diffuse3');
 }
 exports.getFY = getFY;
 
 function getYF(scores) {
-  return countSingleDetWithKey(scores, 'diffuse1', 'YF');
+  return countSingleDetWithKey(scores, 'diffuse2');
 }
 exports.getYF = getYF;
 
 function getY(scores) {
-  return countSingleDetWithKey(scores, 'diffuse1', 'Y');
+  return countSingleDetWithKey(scores, 'diffuse1');
 }
 exports.getY = getY;
 
