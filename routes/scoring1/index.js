63b78e90c4dd2fb1e24e1fea6bb4301b83061e47
lll4592 <ljk7776@gmail.com>
2020-03-29 01:19:38 +0900

    add temp saving for routing

--- routes/scoring1/index.js
+++ routes/scoring1/index.js
@@ -28,4 +28,24 @@ router.route("/scoring1")
         res.redirect("/");
     })
 
+router.route('/scoring1/:id(\\d+)')
+    .get(async(req, res, next) => {
+        var { user } = req.session;
+        var { id: insertId } = req.params;
+        if (!user) {
+            return res.redirect('/login');
+        }
+        if (!insertId) {
+            return res.redirect('/')
+        }
+        var sql = "select score.id as id, test.name as name, test.sex as sex, test.testdate as testdate from score left join test on score.id=test.id";
+
+        var datas = await query.executeSQL(sql);
+
+        var sql2 = "SELECT id, score FROM score WHERE id = ?"
+        var scores = await query.executeSQL(sql2, [insertId]);
+        var score = scores[0] || { score: [] } ;
+
+        res.render('scoring1/index', { datas, score });
+    })
 module.exports = router
